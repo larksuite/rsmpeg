d9acbaba850840053a283b48b979b18c38dbc0dd
liudingming <liudingming@bytedance.com>
2021-05-10 15:17:34 +0800

    Reexport AVCodecID

--- src/avcodec/mod.rs
+++ src/avcodec/mod.rs
@@ -1,10 +1,12 @@
 mod avpicture;
 mod codec;
+mod codec_id;
 mod codec_par;
 mod packet;
 mod parser;
 pub use avpicture::*;
 pub use codec::*;
+pub use codec_id::*;
 pub use codec_par::*;
 pub use packet::*;
 pub use parser::*;
