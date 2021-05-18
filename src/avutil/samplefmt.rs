5306d0dd69a217c9b7bf289ad82f148ac59af596
liudingming <liudingming@bytedance.com>
2021-05-19 03:14:31 +0800

    Better SampleFmt wrapping

--- src/avutil/samplefmt.rs
+++ src/avutil/samplefmt.rs
@@ -1,40 +1,208 @@
 use crate::{ffi, shared::*};
 use std::{
     ffi::CStr,
+    num::NonZeroI32,
     ops::Drop,
     ptr::{self, NonNull},
 };
 
-pub fn av_get_sample_fmt_name(sample_format: i32) -> &'static CStr {
+pub type AVSampleFormat = ffi::AVSampleFormat;
+
+/// Return the name of given sample_fmt, or `None` if sample_fmt is not
+/// recognized.
+///
+/// ```rust
+/// # use rsmpeg::avutil::get_sample_fmt_name;
+/// # use rsmpeg::ffi::AVSampleFormat_AV_SAMPLE_FMT_FLT;
+/// # use std::ffi::CString;
+/// # fn main() {
+/// assert_eq!(
+///     CString::new("flt").ok().as_deref(),
+///     get_sample_fmt_name(AVSampleFormat_AV_SAMPLE_FMT_FLT)
+/// );
+/// # }
+/// ```
+pub fn get_sample_fmt_name(sample_fmt: AVSampleFormat) -> Option<&'static CStr> {
     unsafe {
-        let name = ffi::av_get_sample_fmt_name(sample_format);
-        CStr::from_ptr(name)
+        ffi::av_get_sample_fmt_name(sample_fmt)
+            .upgrade()
+            .map(|x| CStr::from_ptr(x.as_ptr()))
     }
 }
 
-wrap!(AVSamples: *mut u8);
+/// Return a sample format corresponding to name, or None on error.
+///
+/// ```rust
+/// # use rsmpeg::avutil::get_sample_fmt;
+/// # use rsmpeg::ffi::AVSampleFormat_AV_SAMPLE_FMT_FLT;
+/// # use std::ffi::CString;
+/// # fn main() {
+/// assert_eq!(
+///     Some(AVSampleFormat_AV_SAMPLE_FMT_FLT),
+///     get_sample_fmt(&CString::new("flt").unwrap())
+/// );
+/// # }
+/// ```
+pub fn get_sample_fmt(name: &CStr) -> Option<AVSampleFormat> {
+    let sample_fmt = unsafe { ffi::av_get_sample_fmt(name.as_ptr()) };
+    match sample_fmt {
+        ffi::AVSampleFormat_AV_SAMPLE_FMT_NONE => None,
+        _ => Some(sample_fmt),
+    }
+}
+
+/// Get the packed alternative form of the given sample format, return `None` on
+/// error.
+///
+/// i.e. [`AV_SAMPLE_FMT_S16P`](ffi::AVSampleFormat_AV_SAMPLE_FMT_S16P) => [`AV_SAMPLE_FMT_S16`](ffi::AVSampleFormat_AV_SAMPLE_FMT_S16)
+/// ```rust
+/// # use rsmpeg::avutil::get_packed_sample_fmt;
+/// # use rsmpeg::ffi::{AVSampleFormat_AV_SAMPLE_FMT_S16, AVSampleFormat_AV_SAMPLE_FMT_S16P};
+/// # fn main() {
+/// assert_eq!(
+///     Some(AVSampleFormat_AV_SAMPLE_FMT_S16),
+///     get_packed_sample_fmt(AVSampleFormat_AV_SAMPLE_FMT_S16P)
+/// );
+/// # }
+/// ```
+pub fn get_packed_sample_fmt(sample_fmt: AVSampleFormat) -> Option<AVSampleFormat> {
+    let sample_fmt = unsafe { ffi::av_get_packed_sample_fmt(sample_fmt) };
+    match sample_fmt {
+        ffi::AVSampleFormat_AV_SAMPLE_FMT_NONE => None,
+        _ => Some(sample_fmt),
+    }
+}
+
+/// Get the planar alternative form of the given sample format. return `None` on
+/// error.
+///
+/// i.e. [`AV_SAMPLE_FMT_S16`](ffi::AVSampleFormat_AV_SAMPLE_FMT_S16) => [`AV_SAMPLE_FMT_S16P`](ffi::AVSampleFormat_AV_SAMPLE_FMT_S16P)
+/// ```rust
+/// # use rsmpeg::avutil::get_planar_sample_fmt;
+/// # use rsmpeg::ffi::{AVSampleFormat_AV_SAMPLE_FMT_S16, AVSampleFormat_AV_SAMPLE_FMT_S16P};
+/// # fn main() {
+/// assert_eq!(
+///     Some(AVSampleFormat_AV_SAMPLE_FMT_S16P),
+///     get_planar_sample_fmt(AVSampleFormat_AV_SAMPLE_FMT_S16)
+/// );
+/// # }
+/// ```
+pub fn get_planar_sample_fmt(sample_fmt: AVSampleFormat) -> Option<AVSampleFormat> {
+    let sample_fmt = unsafe { ffi::av_get_planar_sample_fmt(sample_fmt) };
+    match sample_fmt {
+        ffi::AVSampleFormat_AV_SAMPLE_FMT_NONE => None,
+        _ => Some(sample_fmt),
+    }
+}
+
+/// Return number of bytes per sample, return `None` when sample format is unknown.
+pub fn get_bytes_per_sample(sample_fmt: AVSampleFormat) -> Option<i32> {
+    NonZeroI32::new(unsafe { ffi::av_get_bytes_per_sample(sample_fmt) }).map(NonZeroI32::get)
+}
+
+wrap! {
+    AVSamples: *mut u8,
+    linesize: i32 = 0,
+    nb_samples: i32 = 0,
+    nb_channels: i32 = 0,
+    sample_fmt: AVSampleFormat = ffi::AVSampleFormat_AV_SAMPLE_FMT_NONE,
+}
 
 impl AVSamples {
-    pub fn new(nb_channels: i32, nb_samples: i32, format: ffi::AVSampleFormat) -> Self {
+    /// Get the required (linesize, buffer_size) for the given audio parameters,
+    /// returns `None` when parameters are invalid.
+    ///
+    /// ```txt
+    /// nb_channels         number of audio channels
+    /// nb_samples          number of samples per channel
+    /// sample_fmt          Audio sample formats
+    /// align               buffer size alignment (0 = default, 1 = no alignment)
+    /// ```
+    pub fn get_buffer_size(
+        nb_channels: i32,
+        nb_samples: i32,
+        sample_fmt: i32,
+        align: i32,
+    ) -> Option<(i32, i32)> {
+        let mut linesize = 0;
+        unsafe {
+            ffi::av_samples_get_buffer_size(
+                &mut linesize,
+                nb_channels,
+                nb_samples,
+                sample_fmt,
+                align,
+            )
+        }
+        .upgrade()
+        .ok()
+        .map(|buffer_size| (linesize, buffer_size))
+    }
+
+    /// Allocate a data pointers array, samples buffer for nb_samples samples,
+    /// and fill data pointers and linesize accordingly.
+    ///
+    /// ```txt
+    /// nb_channels         number of audio channels
+    /// nb_samples          number of samples per channel
+    /// sample_fmt          Audio sample formats
+    /// align               buffer size alignment (0 = default, 1 = no alignment)
+    /// ```
+    pub fn new(nb_channels: i32, nb_samples: i32, sample_fmt: AVSampleFormat, align: i32) -> Self {
         let mut audio_data = ptr::null_mut();
+        let mut linesize = 0;
+
+        // From the documentation, this function only error on no memory, so
+        // unwrap.
         unsafe {
             ffi::av_samples_alloc_array_and_samples(
                 &mut audio_data,
-                ptr::null_mut(),
+                &mut linesize,
                 nb_channels,
                 nb_samples,
-                format,
-                0,
+                sample_fmt,
+                align,
             )
         }
         .upgrade()
         .unwrap();
-        unsafe { AVSamples::from_raw(NonNull::new(audio_data).unwrap()) }
+
+        let mut samples = unsafe { AVSamples::from_raw(NonNull::new(audio_data).unwrap()) };
+        samples.linesize = linesize;
+        samples.nb_channels = nb_channels;
+        samples.nb_samples = nb_samples;
+        samples.sample_fmt = sample_fmt;
+        samples
+    }
+
+    /// Fill an audio buffer with silence.
+    /// `offset` offset in samples at which to start filling.
+    /// `nb_samples` number of samples to fill.
+    pub fn set_silence(&mut self, offset: i32, nb_samples: i32) {
+        let x = unsafe {
+            ffi::av_samples_set_silence(
+                self.as_mut_ptr(),
+                offset,
+                nb_samples,
+                self.nb_channels,
+                self.sample_fmt,
+            )
+        };
+        // From the ffmpeg implementation, `av_samples_set_silence` function
+        // returns nothing but 0, so we can confidently throw the function
+        // output. If this assert is triggered, please file an issue.
+        debug_assert!(x == 0);
     }
 }
 
 impl Drop for AVSamples {
     fn drop(&mut self) {
+        // Documentation states:
+        //
+        // The allocated samples buffer can be freed by using av_freep(&audio_data[0])
+        // Allocated data will be initialized to silence.
+        // So we first free the audio_data[0].
+        // then free the audio_data array(since it's allocated by `av_samples_alloc_array_and_samples`).
         unsafe { ffi::av_free(*self.as_mut_ptr() as _) }
         unsafe { ffi::av_free(self.as_mut_ptr() as _) }
     }
