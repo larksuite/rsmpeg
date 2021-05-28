8aed696b9553a504be1a36a8acd7b1b253a2a4ff
Donough Liu <liudingming@bytedance.com>
2021-05-28 20:35:38 +0800

    Preseve compatibility with latest FFmpeg (#21)

--- src/avcodec/mod.rs
+++ src/avcodec/mod.rs
@@ -1,12 +1,10 @@
 //! Everything related to `libavcodec`.
-mod avpicture;
 mod codec;
 mod codec_id;
 mod codec_par;
 mod packet;
 mod parser;
 
-pub use avpicture::*;
 pub use codec::*;
 pub use codec_id::*;
 pub use codec_par::*;
