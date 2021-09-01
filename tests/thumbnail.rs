38c02fe9139c3b6117b267a209589f8a64e97f74
南浦月 <nanpuyue@gmail.com>
2021-09-02 00:22:15 +0800

    chore: simplify the code in thumbnail.rs (#65)
    * chore: simplify the code in thumbnail.rs
    
    * fix: get cover_frame for short video

--- tests/thumbnail.rs
+++ tests/thumbnail.rs
@@ -1,6 +1,6 @@
 use rsmpeg::{avcodec::*, avformat::*, avutil::*, error::RsmpegError, ffi, swscale::*};
 
-use anyhow::{Context, Result};
+use anyhow::{bail, Context, Result};
 use cstr::cstr;
 use std::{
     ffi::CStr,
@@ -33,24 +33,23 @@ fn thumbnail(
     };
 
     let cover_frame = loop {
-        let cover_packet = {
-            let mut cover_packet = None;
-            while let Some(packet) = input_format_context.read_packet()? {
-                // Get first video packet.
-                if packet.stream_index == video_stream_index as i32 {
-                    cover_packet = Some(packet);
-                    break;
-                }
+        let cover_packet = loop {
+            match input_format_context.read_packet()? {
+                Some(x) if x.stream_index != video_stream_index as i32 => {}
+                x => break x,
             }
-            cover_packet.context("Cannnot find video cover packet")?
         };
 
-        decode_context.send_packet(Some(&cover_packet))?;
+        decode_context.send_packet(cover_packet.as_ref())?;
         // repeatedly send packet until a frame can be extracted
         match decode_context.receive_frame() {
             Ok(x) => break x,
             Err(RsmpegError::DecoderDrainError) => {}
             Err(e) => return Err(e.into()),
+        }
+
+        if cover_packet.is_none() {
+            bail!("Can't find video cover frame");
         }
     };
 
