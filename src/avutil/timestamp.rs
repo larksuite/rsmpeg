a04e401436f54a76de2387456dc644c4c8bbedf5
Donough Liu <liudingming@bytedance.com>
2024-01-07 00:26:04 +0800

    Add `ts2str` and `ts2timestr` (#133)

---
+++ src/avutil/timestamp.rs
@@ -0,0 +1,22 @@
+use crate::ffi::av_q2d;
+use crate::ffi::AVRational;
+use crate::ffi::AV_NOPTS_VALUE;
+use std::ffi::c_double;
+
+/// Get a string containing a timestamp representation.
+pub fn ts2str(ts: i64) -> String {
+    if ts == AV_NOPTS_VALUE {
+        "NOPTS".to_string()
+    } else {
+        ts.to_string()
+    }
+}
+
+/// Get a string containing a timestamp time representation.
+pub fn ts2timestr(ts: i64, tb: AVRational) -> String {
+    if ts == AV_NOPTS_VALUE {
+        "NOPTS".to_string()
+    } else {
+        format!("{:.6}", av_q2d(tb) * ts as c_double)
+    }
+}
