21fc8ea97067547616a6f9bc6a1dd831ff2cf97d
ldm0 <ldm2993593805@163.com>
2024-01-07 12:44:41 -0800

    Revise `decode_audio`, `decode_vidio` example
    * Rename `AVCodecParserContext::find` to `AVCodecParserContext::init`
    
    * Rename `is_planar`to `sample_fmt_is_planar`

--- tests/decode_audio.rs
+++ tests/decode_audio.rs
@@ -1,19 +1,20 @@
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use rsmpeg::{
     avcodec::{AVCodecContext, AVCodecParserContext, AVPacket},
     avformat::AVFormatContextInput,
     avutil::{
-        get_bytes_per_sample, get_packed_sample_fmt, get_sample_fmt_name, is_planar, AVFrame,
-        AVSampleFormat,
+        get_bytes_per_sample, get_packed_sample_fmt, get_sample_fmt_name, sample_fmt_is_planar,
+        AVFrame, AVSampleFormat,
     },
     error::RsmpegError,
     ffi,
 };
-use std::ffi::CString;
+use rusty_ffmpeg::ffi::AV_INPUT_BUFFER_PADDING_SIZE;
 use std::fs::{self, File};
 use std::io::Write;
 use std::path::Path;
 use std::slice::from_raw_parts;
+use std::{ffi::CString, io::Read};
 
 fn get_format_from_sample_fmt(sample_fmt: AVSampleFormat) -> Option<&'static str> {
     let sample_fmt_entries = [
@@ -50,7 +51,8 @@ fn decode(
         .send_packet(packet)
         .context("Send packet failed.")?;
     let channels = decode_context
-        .channels
+        .ch_layout
+        .nb_channels
         .try_into()
         .context("channels overflow")?;
     let sample_fmt = decode_context.sample_fmt;
@@ -67,6 +69,8 @@ fn decode(
 }
 
 fn decode_audio(audio_path: &str, out_file_path: &str) -> Result<()> {
+    const AUDIO_INBUF_SIZE: usize = 20480;
+
     let (decoder, mut decode_context) = {
         // safety, &str ensures no internal null bytes.
         let audio_path = CString::new(audio_path).unwrap();
@@ -86,47 +90,59 @@ fn decode_audio(audio_path: &str, out_file_path: &str) -> Result<()> {
                     .codecpar(),
             )
             .context("Apply codecpar failed.")?;
-        decode_context
-            .open(None)
-            .context("Open codec context failed.")?;
+        decode_context.open(None).context("Could not open codec")?;
         input_format_context.dump(stream_index, &audio_path)?;
         (decoder, decode_context)
     };
 
-    let audio_data = fs::read(audio_path).context("Read audio file failed.")?;
-    fs::create_dir_all(Path::new(out_file_path).parent().unwrap())
-        .context("Create out file dir failed.")?;
+    let mut inbuf = [0u8; AUDIO_INBUF_SIZE + AV_INPUT_BUFFER_PADDING_SIZE as usize];
+
+    let mut audio_file =
+        File::open(audio_path).with_context(|| anyhow!("Could not open {}", audio_path))?;
+    fs::create_dir_all(Path::new(out_file_path).parent().unwrap()).unwrap();
     let out_file = File::create(out_file_path).context("Open out file failed.")?;
 
-    let mut parsed_offset = 0;
-    let mut parser_context =
-        AVCodecParserContext::find(decoder.id).context("Find parser context failed.")?;
-
+    let mut parser_context = AVCodecParserContext::init(decoder.id).context("Parser not found")?;
     let mut packet = AVPacket::new();
-    while parsed_offset < audio_data.len() {
-        let (get_packet, offset) = parser_context
-            .parse_packet(
-                &mut decode_context,
-                &mut packet,
-                &audio_data[parsed_offset..],
-            )
-            .context("Parse packet failed.")?;
-        if get_packet {
-            decode(&mut decode_context, Some(&packet), &out_file).context("Decode failed.")?;
+
+    loop {
+        let len = audio_file
+            .read(&mut inbuf[..AUDIO_INBUF_SIZE])
+            .context("Read input file failed.")?;
+        if len == 0 {
+            break;
         }
-        parsed_offset += offset;
+        let mut parsed_offset = 0;
+        while parsed_offset < len {
+            let (get_packet, offset) = parser_context
+                .parse_packet(&mut decode_context, &mut packet, &inbuf[parsed_offset..len])
+                .context("Error while parsing")?;
+            parsed_offset += offset;
+            if get_packet {
+                decode(&mut decode_context, Some(&packet), &out_file)?;
+            }
+        }
     }
 
-    decode(&mut decode_context, None, &out_file).context("Flush decode context failed.")?;
+    // Flush parser
+    let (get_packet, _) = parser_context
+        .parse_packet(&mut decode_context, &mut packet, &[])
+        .context("Error while parsing")?;
+    if get_packet {
+        decode(&mut decode_context, Some(&packet), &out_file)?;
+    }
+
+    // Flush decoder
+    decode(&mut decode_context, None, &out_file)?;
 
     let mut sample_fmt = decode_context.sample_fmt;
 
-    if is_planar(sample_fmt) {
-        let name = get_sample_fmt_name(sample_fmt).context("Unknown sample fmt")?;
+    if sample_fmt_is_planar(sample_fmt) {
+        let name = get_sample_fmt_name(sample_fmt);
         println!(
             "Warning: the sample format the decoder produced is planar \
             ({}). This example will output the first channel only.",
-            name.to_str().unwrap()
+            name.map(|x| x.to_str().unwrap()).unwrap_or("?")
         );
         sample_fmt = get_packed_sample_fmt(sample_fmt).context("Cannot get packed sample fmt")?;
     }
@@ -136,7 +152,7 @@ fn decode_audio(audio_path: &str, out_file_path: &str) -> Result<()> {
     println!("Play the output audio file with the command:");
     println!(
         "ffplay -f {} -ac {} -ar {} {}",
-        fmt, decode_context.channels, decode_context.sample_rate, out_file_path
+        fmt, decode_context.ch_layout.nb_channels, decode_context.sample_rate, out_file_path
     );
     Ok(())
 }
