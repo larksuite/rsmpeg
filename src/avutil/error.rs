26982d61f02e13c35f50dac52cf31d9fbeeeb571
ldm0 <ldm2993593805@163.com>
2024-01-07 11:06:23 -0800

    Add `err2str` mirroring `av_err2str`

---
+++ src/avutil/error.rs
@@ -0,0 +1,18 @@
+use crate::ffi;
+use rusty_ffmpeg::ffi::AV_ERROR_MAX_STRING_SIZE;
+use std::ffi::{c_int, CStr};
+
+/// Return a description of the AVERROR code errnum.
+///
+/// Return `Some(description)` on success, a negative value if a description for
+/// errnum cannot be found.
+pub fn err2str(errnum: c_int) -> Option<String> {
+    const ERRBUF_SIZE: usize = AV_ERROR_MAX_STRING_SIZE as usize;
+    let mut errbuf = [0u8; ERRBUF_SIZE];
+    if unsafe { ffi::av_strerror(errnum, errbuf.as_mut_ptr() as _, ERRBUF_SIZE) } == 0 {
+        let result = CStr::from_bytes_until_nul(&errbuf).unwrap();
+        Some(result.to_string_lossy().into_owned())
+    } else {
+        None
+    }
+}
