3bbe229871942830bb6f2e266cd02e6ede2ee73c
Yesterday17 <mmf@mmf.moe>
2025-06-08 22:31:41 +0800

    Detect `target_env = msvc` for `codec_id`
    Make `rsmpeg` compile on `x86_64-pc-windows-gnu`

--- src/avcodec/parser.rs
+++ src/avcodec/parser.rs
@@ -12,7 +12,7 @@ impl AVCodecParserContext {
     pub fn init(codec_id: AVCodecID) -> Option<Self> {
         // On Windows enum is i32, On *nix enum is u32.
         // ref: https://github.com/rust-lang/rust-bindgen/issues/1361
-        #[cfg(not(windows))]
+        #[cfg(not(target_env = "msvc"))]
         let codec_id = codec_id as i32;
         unsafe { ffi::av_parser_init(codec_id) }
             .upgrade()
